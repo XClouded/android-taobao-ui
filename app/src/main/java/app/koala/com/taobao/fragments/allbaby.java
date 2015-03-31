b1d3a4e5835e99d3c7d64c62f5bcd48cc9114846
fachilles <piratechao@gmail.com>
2015-03-31 13:44:57 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	.gitignore

--- app/src/main/java/app/koala/com/taobao/fragments/allbaby.java
+++ app/src/main/java/app/koala/com/taobao/fragments/allbaby.java
@@ -5,15 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import app.koala.com.taobao.R;
+import app.koala.com.taobao.adapter.Adapter_ListView_cart;
+import app.koala.com.taobao.tools.IBtnCallListener;
 
 /**
  * Created by v-rosong on 2015/3/30.
  */
-public class allbaby extends Fragment implements View.OnClickListener {
+public class allbaby extends Fragment implements  View.OnClickListener,IBtnCallListener,Adapter_ListView_cart.onCheckedChanged {
 
-    private String str_del = "";
+    IBtnCallListener btnCallListener;
+    private TextView tv_goShop,tv_cart_Allprice,tv_cart_buy_Ordel;
+    private LinearLayout ll_cart;
+    private ListView listView_cart;
+    private CheckBox cb_cart_all;
+    private Adapter_ListView_cart adapter;
+    private String str_del = "结算(0)";
+    private boolean[] is_choice;
 
     public allbaby(){
 
@@ -35,7 +49,51 @@ public class allbaby extends Fragment implements View.OnClickListener {
 
     }
 
-    private void initView(View view){
+    @Override
+    public void transferMsg() {
 
     }
+
+    @Override
+    public void getChoiceData(int position, boolean isChoice) {
+
+    }
+
+    private void initView(View view){
+        this.tv_goShop = (TextView)view.findViewById(R.id.tv_goShop);
+        this.tv_cart_Allprice = (TextView)view.findViewById(R.id.tv_cart_Allprice);
+
+        this.tv_cart_buy_Ordel = (TextView)view.findViewById(R.id.tv_cart_buy_or_del);
+        this.tv_cart_buy_Ordel.setText(this.str_del);
+
+        this.cb_cart_all = (CheckBox)view.findViewById(R.id.cb_cart_all);
+        this.cb_cart_all.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int isChoice_all = 0;
+                if(isChecked){}
+            }
+        });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
